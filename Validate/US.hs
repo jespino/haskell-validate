dadd3e7bd6ce7aebedf57563aa2e8ddf18df3936
Jesús Espino <jespinog@gmail.com>
2013-12-30 23:52:50 +0100

    Adding first US validator

---
+++ Validate/US.hs
@@ -0,0 +1,4 @@
+module Validate.US where
+
+region :: String -> Bool
+region reg = reg `elem` ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
