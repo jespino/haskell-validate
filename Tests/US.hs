76327b3c8165ef6b8282cc4edab4c85cb4fd9db3
Jesús Espino <jesus.espino@kaleidos.net>
2013-12-31 12:54:51 +0100

    Working some tests of US validation

--- Tests/US.hs
+++ Tests/US.hs
@@ -1,33 +1,44 @@
 module Tests.US where
 
 import Validate.US
-import HUnit
+import Test.HUnit
 
-region1 = assertEqual "" True (region "MT")
-region2 = assertEqual "" True (region "DC")
-region3 = assertEqual "" True (region "il")
-region4 = assertEqual "" False (region "ILL")
-region5 = assertEqual "" False (region "FLA")
-region6 = assertEqual "" False (region "NL")
 
--- Should work
-ssn1 = assertEqual "" True (ssn "712180565")
-ssn2 = assertEqual "" True (ssn "019880001")
-ssn3 = assertEqual "" True (ssn "019889999")
-ssn4 = assertEqual "" True (ssn "133920565")
-ssn5 = assertEqual "" True (ssn "001020030")
-ssn6 = assertEqual "" True (ssn "097920845")
-ssn7 = assertEqual "" True (ssn "097469490")
-ssn8 = assertEqual "" True (ssn "001-01-3000")
-ssn9 = assertEqual "" True (ssn "001 42 3000")
-ssn10 = assertEqual "" True (ssn "001 44 3000")
+tests_US_region = TestList [
+              TestLabel "region1" $ TestCase $ assertEqual "" True (region "MT"),
+              TestLabel "region2" $ TestCase $ assertEqual "" True (region "DC"),
+              TestLabel "region3" $ TestCase $ assertEqual "" True (region "il"),
+              TestLabel "region4" $ TestCase $ assertEqual "" False (region "ILL"),
+              TestLabel "region5" $ TestCase $ assertEqual "" False (region "FLA"),
+              TestLabel "region6" $ TestCase $ assertEqual "" False (region "NL"),
+              TestLabel "region1-strict" $ TestCase $ assertEqual "" True (regionStrict "MT"),
+              TestLabel "region2-strict" $ TestCase $ assertEqual "" True (regionStrict "DC"),
+              TestLabel "region3-strict" $ TestCase $ assertEqual "" False (regionStrict "il"),
+              TestLabel "region4-strict" $ TestCase $ assertEqual "" False (regionStrict "ILL"),
+              TestLabel "region5-strict" $ TestCase $ assertEqual "" False (regionStrict "FLA"),
+              TestLabel "region6-strict" $ TestCase $ assertEqual "" False (regionStrict "NL")
+           ]
 
--- Shouldn't work
-ssn11 = assertEqual "" False (ssn "001 43 3000")
-ssn12 = assertEqual "" False (ssn "019880000")
-ssn13 = assertEqual "" False (ssn "712194509")
-ssn14 = assertEqual "" False (ssn "019000000")
-ssn15 = assertEqual "" False (ssn "019890000")
-ssn16 = assertEqual "" False (ssn "001713000")
-ssn17 = assertEqual "" False (ssn "133939759")
-ssn18 = assertEqual "" False (ssn "097999490")
+tests_US_ssn = TestList [
+                -- Should work
+                TestLabel "ssn1" $ TestCase $ assertEqual "" True (ssn "712180565"),
+                TestLabel "ssn2" $ TestCase $ assertEqual "" True (ssn "019880001"),
+                TestLabel "ssn3" $ TestCase $ assertEqual "" True (ssn "019889999"),
+                TestLabel "ssn4" $ TestCase $ assertEqual "" True (ssn "133920565"),
+                TestLabel "ssn5" $ TestCase $ assertEqual "" True (ssn "001020030"),
+                TestLabel "ssn6" $ TestCase $ assertEqual "" True (ssn "097920845"),
+                TestLabel "ssn7" $ TestCase $ assertEqual "" True (ssn "097469490"),
+                TestLabel "ssn8" $ TestCase $ assertEqual "" True (ssn "001-01-3000"),
+                TestLabel "ssn9" $ TestCase $ assertEqual "" True (ssn "001 42 3000"),
+                TestLabel "ssn10" $ TestCase $ assertEqual "" True (ssn "001 44 3000"),
+
+                -- Shouldn't work
+                TestLabel "ssn11" $ TestCase $ assertEqual "" False (ssn "001 43 3000"),
+                TestLabel "ssn12" $ TestCase $ assertEqual "" False (ssn "019880000"),
+                TestLabel "ssn13" $ TestCase $ assertEqual "" False (ssn "712194509"),
+                TestLabel "ssn14" $ TestCase $ assertEqual "" False (ssn "019000000"),
+                TestLabel "ssn15" $ TestCase $ assertEqual "" False (ssn "019890000"),
+                TestLabel "ssn16" $ TestCase $ assertEqual "" False (ssn "001713000"),
+                TestLabel "ssn17" $ TestCase $ assertEqual "" False (ssn "133939759"),
+                TestLabel "ssn18" $ TestCase $ assertEqual "" False (ssn "097999490")
+           ]
--- Validate/US.hs
+++ Validate/US.hs
@@ -1,4 +1,14 @@
 module Validate.US where
 
+import Data.Char
+
+regionStrict :: String -> Bool
+regionStrict reg = reg `elem` ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+-- Strict parameter
 region :: String -> Bool
-region reg = reg `elem` ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+region reg = region $ map toUpper reg
+
+-- Not implemented
+ssn :: String -> Bool
+ssn code = False
